105ba1df8018f1f7c261c50909f9b0ecb28a563f
Brendan Cox <bcox@saucelabs.com>
2015-09-22 10:46:02 -0700

    added some argument validation, bit of tidying

--- sync_file.sh
+++ sync_file.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-# this only works on a mac, just need to change
+# this only works on a mac, you'll need to change
 # calls to stat and date to get it working on linux
 
-sync_file() {
+do_sync() {
     src_file=$1
     dest_location=$2
     echo "$src_file $dest_location"
     scp $src_file $dest_location
 }
 
+if [ $# -lt 2 ]; then
+    script_name=`basename $0`
+    echo "Usage: $script_name source_file destination"
+    exit 1
+fi
+
 src=$1
 dest=$2
+if [ ! -f "$src" ]; then
+    "Could not find $src, are you in the correct directory?"
+    exit 2
+fi
 
 last_sync=0
 while true; do
     mtime=`stat -f "%m" $src`
     if [ "$mtime" -gt "$last_sync" ]; then
-	sync_file $src $dest
+	do_sync $src $dest
 	last_sync=`date "+%s"`
     fi
     sleep 2
